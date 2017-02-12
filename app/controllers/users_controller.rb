1826b473b6e0f72aad4d4acb0cf776949331c50c
Phan Nguyen Tri <phnguyentri@gmail.com>
2017-02-12 11:13:38 +0700

    Chaprer 8: Basic Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,15 @@ class UsersController < ApplicationController
   end
 
   def show
-	@user = User.find(params[:id])
+	  @user = User.find(params[:id])
+    # binding.pry
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -24,4 +26,5 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    
 end
